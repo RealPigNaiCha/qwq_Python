c9852e6ce66294977131c60b96616a255ea17b38
RealPigNaiCha <37406294+RealPigNaiCha@users.noreply.github.com>
2024-02-29 00:55:34 +0800

    Add files via upload
    2024/02/28，创建仓库，添加了几个示例程序

---
+++ calCircleArea.py
@@ -0,0 +1,8 @@
+'''The first python demo
+    Created in 2024/2/28/10:47
+    Modified in 2024/2/28/10:57
+'''
+radious=eval(input("输入圆的半径："))
+area=3.14159*radious*radious
+print("圆的精确面积是"+str(area))
+print("圆的面积是{:.2f}".format(area))
---
+++ tempConvert.py
@@ -0,0 +1,11 @@
+'''tempConvert.py
+Created in 2024/2/28/11:14'''
+tempStr=input("请输入带有符号的温度值：")
+if tempStr[-1] in ['F','f']:
+    C=(eval(tempStr[0:-1])-32)/1.8
+    print("转换后的温度是{:.2f}C".format(C))
+elif tempStr[-1] in ["C","c"]:
+    F=1.8*eval(tempStr[0:-1])+32
+    print("转换后的温度是{:.2f}F".format(F))
+else:
+    print("输入格式错误！")
