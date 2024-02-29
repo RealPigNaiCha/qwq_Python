4f3074856c68f4d2b4bfdbd913ba2a30d6c5a596
RealPigNaiCha <realpiganicha@qq.com>
2024-02-29 21:59:03 +0800

    drawRedStar.py新名称，添加drawPython绘制随机蟒蛇，添加单行计算器singleLine_calculator.py

---
+++ drawPython.py
@@ -0,0 +1,21 @@
+"""draw random python 
+created in 2024/02/29"""
+from turtle import *
+import random
+pythonLen=random.randint(1,10)
+setup(150*pythonLen,600)
+colorList=["red","cyan","green","blue","yellow","black","magenta","seashell","gold","pink","purple","tomato"]
+penup()
+forward(-120*pythonLen/2)
+pendown()
+pensize(25)
+seth(-40)
+for i in range(pythonLen):
+    pencolor(colorList[random.randint(0,len(colorList)-1)])
+    circle(40,80)
+    circle(-40,80)
+circle(40,80/2)
+forward(40)
+circle(16,180)
+forward(40*2/3)
+done()
---
+++ drawRedStar.py
@@ -0,0 +1,11 @@
+'''Turtle demo created in 2024/2/28/10:51'''
+from turtle import *
+fillcolor("red")
+begin_fill()
+while True :
+    forward(200)
+    right(144)
+    if abs(pos()) < 1 :
+        break
+end_fill()
+done()
